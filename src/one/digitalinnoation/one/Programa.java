769568e438b26e619b12bad16cefc251eba24f6a
Karoline Barão <54177866+KarolineBarao@users.noreply.github.com>
2022-05-30 17:50:44 -0300

    Add files via upload

---
+++ src/one/digitalinnoation/one/Programa.java
@@ -0,0 +1,59 @@
+package one.digitalinnoation.one;
+
+
+import facade.Facade;
+import singleton.SingletonEager;
+import singleton.SingletonLazy;
+import singleton.SingletonLazyHolder;
+import strategy.Comportamento;
+import strategy.ComportamentoAgressivo;
+import strategy.ComportamentoDefensivo;
+import strategy.ComportamentoNormal;
+import strategy.Robo;
+
+public class Programa {
+
+	public static void main(String[] args) {
+	
+		// Singleton
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		
+		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		System.out.println(" ");
+		
+		// Stategy
+	Comportamento normal = new ComportamentoNormal();
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(agressivo);
+		robo.mover();
+		robo.setComportamento(defensivo);
+		robo.mover();
+		
+		System.out.println(" ");
+		
+		//Facade
+		Facade facade = new Facade();
+		facade.migrarCliente("Karoline", "88750000");
+		
+
+	}
+
+}
